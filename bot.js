09d6956a80925c1aa088559f5d808fd0047f4017
Marat <mrmar99@icloud.com>
2023-10-24 19:51:18 +0300

    Change telegraf to grammy framework

--- bot.js
+++ bot.js
@@ -1,10 +1,21 @@
-require("dotenv").config();
-const { Telegraf } = require('telegraf');
-
-const bot = new Telegraf(process.env.TOKEN);
-console.log(bot)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const grammy_1 = require("grammy");
+const { TOKEN: token = '' } = process.env;
+console.log(token);
+// Create an instance of the `Bot` class and pass your bot token to it.
+const bot = new grammy_1.Bot(token); // <-- put your bot token between the ""
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Handle other messages.
+bot.on("message", (ctx) => {
+    const { message } = ctx.update;
+    if (message.dice && message.dice.emoji === '🎲') {
+        console.log(message.dice);
+    }
+    else {
+        ctx.reply("Кинь кость");
+    }
+});
+bot.start();
